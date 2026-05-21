72c2eededca9edeb7e4976074d1bb0acd7d01ae5
phamminh-coder33 <phamminh12012009@gmail.com>
2026-05-21 20:12:02 +0700

    Update cau1.py

--- cau1.py
+++ cau1.py
@@ -1,1 +1,11 @@
-
+tong = 0
+n = int(input("Nhập số n: "))
+tong = tong + n % 10
+n = n // 10
+tong = tong + n % 10
+n = n // 10
+tong = tong + n % 10
+n = n // 10
+tong = tong + n % 10
+n = n // 10
+print("Tổng là ",tong)
