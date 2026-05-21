2f8dd9ded87780f3421e3eada6ce15e324ac0955
phamminh-coder33 <phamminh12012009@gmail.com>
2026-05-21 20:43:04 +0700

    Update cau4.py

--- cau4.py
+++ cau4.py
@@ -1,1 +1,10 @@
-
+cannang = float(input(" cân nặng là "))
+chieucao = float(input(" chiều cao là "))
+BMI = cannang / (chieucao ** 2)
+print("BMI của bạn là ",BMI)
+if BMI < 18.5:
+ print("Gầy")
+if 18.5 <= BMI < 25:
+ print("Bình Thường")
+if BMI > 25:
+ print("Thừa cân")
