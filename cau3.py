87d347b0f6536ad068b5ad6a74d67b0ff9ee6c1f
phamminh-coder33 <phamminh12012009@gmail.com>
2026-05-21 20:32:31 +0700

    Update cau3.py

--- cau3.py
+++ cau3.py
@@ -1,1 +1,10 @@
-
+lythuyet = float(input("Điểm Lý Thuyết: "))
+thuchanh = float(input("Điểm Thực Hành: "))
+tong = lythuyet + thuchanh
+if tong >= 10:
+ if lythuyet > 0:
+   if thuchanh > 0:
+     print("Đậu")
+else:
+        print("Rớt")
+  
